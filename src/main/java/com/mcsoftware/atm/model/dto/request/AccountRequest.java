a18e77c0a5515a1e5de4361bd1ad594d4c62a1d3
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-21 17:11:08 +0700

    [INIT] Account Dto

---
+++ src/main/java/com/mcsoftware/atm/model/dto/request/AccountRequest.java
@@ -0,0 +1,20 @@
+package com.mcsoftware.atm.model.dto.request;
+
+import com.mcsoftware.atm.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class AccountRequest {
+    private String id;
+    private String accountNumber;
+    private BigDecimal balance;
+    private User user;
+}
---
+++ src/main/java/com/mcsoftware/atm/model/dto/response/AccountResponse.java
@@ -0,0 +1,19 @@
+package com.mcsoftware.atm.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class AccountResponse {
+    private String id;
+    private String accountNumber;
+    private BigDecimal balance;
+    private String userId;
+}

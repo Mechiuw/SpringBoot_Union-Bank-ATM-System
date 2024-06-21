7638e7fe0938f74cca06dd98fc6956f61b6e9c66
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-21 16:20:08 +0700

    [INIT] Initialized files

---
+++ src/test/java/com/mcsoftware/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcsoftware.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

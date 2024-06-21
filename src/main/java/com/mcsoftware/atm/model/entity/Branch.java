7628e5110f16930dec2fc9ee133469dd82bd01b7
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-21 16:51:28 +0700

    [INIT] Branch ENtity

--- src/main/java/com/mcsoftware/atm/model/entity/Branch.java
+++ src/main/java/com/mcsoftware/atm/model/entity/Branch.java
@@ -1,4 +1,39 @@
 package com.mcsoftware.atm.model.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "m_branch")
 public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name ="branch_name" ,nullable = false)
+    private String name;
+
+    @Column(name = "branch_location",nullable = false)
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "bank_id")
+    private Bank bank;
+
+    @OneToMany(mappedBy = "branch",cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private List<ATM> atms;
+
 }

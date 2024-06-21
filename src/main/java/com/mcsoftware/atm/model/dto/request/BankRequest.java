cf824f91cd0af57d117bc0d6d93b678ee01301a9
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-21 17:15:15 +0700

    [INIT] Bank DTO

---
+++ src/main/java/com/mcsoftware/atm/model/dto/request/BankRequest.java
@@ -0,0 +1,18 @@
+package com.mcsoftware.atm.model.dto.request;
+
+import com.mcsoftware.atm.model.entity.Branch;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class BankRequest {
+    private String name;
+    private List<BranchRequest> branchList;
+}
---
+++ src/main/java/com/mcsoftware/atm/model/dto/response/BankResponse.java
@@ -0,0 +1,19 @@
+package com.mcsoftware.atm.model.dto.response;
+
+import com.mcsoftware.atm.model.entity.Branch;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class BankResponse {
+    private String id;
+    private String name;
+    private List<Branch> branchList;
+}

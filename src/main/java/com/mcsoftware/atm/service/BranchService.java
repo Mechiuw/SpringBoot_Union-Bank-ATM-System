aa5ee0bacd38e287379aab6a2cea1c10ea01c74f
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-24 21:13:00 +0700

    [FEATURE] Added BranchGroupingResponse for responses

---
+++ src/main/java/com/mcsoftware/atm/service/BranchService.java
@@ -0,0 +1,20 @@
+package com.mcsoftware.atm.service;
+
+import com.mcsoftware.atm.model.dto.request.BranchRequest;
+import com.mcsoftware.atm.model.dto.response.BranchGroupingResponse;
+import com.mcsoftware.atm.model.dto.response.BranchResponse;
+import com.mcsoftware.atm.model.entity.ATM;
+import com.mcsoftware.atm.model.entity.Branch;
+
+import java.util.List;
+
+public interface BranchService {
+    BranchResponse create(BranchRequest branchRequest);
+    BranchResponse getById(String id);
+    List<Branch> getAll();
+    BranchResponse update(String id,BranchRequest branchRequest);
+    void delete(String id);
+    List<ATM> getAllAtm(String branchId);
+    BranchResponse bankReference(String branchId);
+    BranchGroupingResponse groupedBranchLocations();
+}

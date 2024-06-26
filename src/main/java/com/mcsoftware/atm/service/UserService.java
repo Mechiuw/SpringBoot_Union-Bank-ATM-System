2b505d69e035f05cca3200d210de2b68433d9ee8
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-27 01:20:08 +0700

    [FEATURE] Mapped UserServices

---
+++ src/main/java/com/mcsoftware/atm/service/UserService.java
@@ -0,0 +1,18 @@
+package com.mcsoftware.atm.service;
+
+import com.mcsoftware.atm.model.dto.request.UserRequest;
+import com.mcsoftware.atm.model.dto.response.UserResponse;
+import com.mcsoftware.atm.model.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponse create(UserRequest userRequest);
+    UserResponse getById(String id);
+    List<User> getAll(UserRequest userRequest);
+    UserResponse update(String id, UserRequest userRequest);
+    void delete(String id);
+    UserResponse softDelete(String id);
+    UserResponse blockUser(String id);
+    User validateUser(User user);
+}

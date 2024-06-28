0389ce1ded0bcaa7ef9ddf67211823023ad6d46c
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-28 10:11:28 +0700

    [IMPROVE] Added anotations to each functions in user controller

--- src/main/java/com/mcsoftware/atm/controller/UserController.java
+++ src/main/java/com/mcsoftware/atm/controller/UserController.java
@@ -5,19 +5,32 @@ import com.mcsoftware.atm.model.dto.request.UserRequest;
 import com.mcsoftware.atm.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(AppPath.USER)
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    public ResponseEntity<?> create(UserRequest userRequest){}
-    public ResponseEntity<?> getById(UserRequest userRequest){}
-    public ResponseEntity<?> getAll(UserRequest userRequest){}
-    public ResponseEntity<?> update(UserRequest userRequest){}
-    public void delete(UserRequest userRequest){}
-    public ResponseEntity<?> softDelete(String id){}
-    public ResponseEntity<?> blockUser(String id){}
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody UserRequest userRequest){}
+
+    @GetMapping(AppPath.GET_BY_ID)
+    public ResponseEntity<?> getById(@PathVariable String id){}
+
+    @GetMapping(AppPath.GET_ALL)
+    public ResponseEntity<?> getAll(){}
+
+    @PutMapping(AppPath.PUT_BY_ID)
+    public ResponseEntity<?> update(@PathVariable String id,@RequestBody UserRequest userRequest){}
+
+    @DeleteMapping(AppPath.DELETE_BY_ID)
+    public void delete(@PathVariable String id){}
+
+    @PutMapping(AppPath.SOFT_DELETE_BY_ID)
+    public ResponseEntity<?> softDelete(@PathVariable String id){}
+
+    @PutMapping("/block/{id}")
+    public ResponseEntity<?> blockUser(@PathVariable String id){}
 }
